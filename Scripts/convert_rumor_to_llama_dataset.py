4715099bd657b3b9d729f44b2c4515bb1ebb49cc
sawyerbutton <linghui92liu@gmail.com>
2023-07-29 16:18:18 +0800

    update convert conent logic and create a new dataset

--- Scripts/convert_rumor_to_llama_dataset.py
+++ Scripts/convert_rumor_to_llama_dataset.py
@@ -11,12 +11,13 @@ with open('./Data/fact.csv', 'r', encoding='utf-8') as csvfile:
     for row in reader:
         title = row['title']
         abstract = row['abstract']
+        explain = row['explain']
         
         # 构建json对象
         json_obj = {
             "instruction": title + '请判断上述内容是以下四种谣言分类中的哪一种（确实如此，尚无定论，伪科学，伪常识），并以step by step的方式解释其判断过程。',
             "input": "",
-            "output": abstract
+            "output": '上述言论属于' + explain + ',因为' + abstract
         }
         
         json_list.append(json_obj)
